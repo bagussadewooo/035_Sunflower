9b2787f2bf0d171532124ac6c347dc0dde648065
bagussadewooo <sadewo1248@gmail.com>
2023-10-18 17:07:40 +0700

    Mengubah tampilan MyGarden menjadi Scrollable Horizontal List

--- app/src/main/java/com/google/samples/apps/sunflower/compose/garden/GardenScreen.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/compose/garden/GardenScreen.kt
@@ -24,8 +24,9 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.material3.Button
@@ -96,8 +97,8 @@ private fun GardenList(
     // Call reportFullyDrawn when the garden list has been rendered
     val gridState = rememberLazyGridState()
     ReportDrawnWhen { gridState.layoutInfo.totalItemsCount > 0 }
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(1),
         modifier,
         state = gridState,
         contentPadding = PaddingValues(
@@ -130,13 +131,18 @@ private fun GardenListItem(
 
     ElevatedCard(
         onClick = { onPlantClick(plant) },
-        modifier = Modifier.padding(
-            start = cardSideMargin,
-            end = cardSideMargin,
-            bottom = dimensionResource(id = R.dimen.card_bottom_margin)
-        ),
-        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.secondaryContainer)
-    ) {
+        modifier = Modifier
+            .size(
+                width = dimensionResource(id =R.dimen.card_width) ,
+                height = dimensionResource(id =R.dimen.card_height)
+            )
+            .padding(
+                start = cardSideMargin,
+                end = cardSideMargin,
+                bottom = dimensionResource(id = R.dimen.card_bottom_margin1)
+            ),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.secondaryContainer),
+        ) {
         Column(Modifier.fillMaxWidth()) {
             SunflowerImage(
                 model = vm.imageUrl,
